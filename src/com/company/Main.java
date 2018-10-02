07e0c95c4d596e3372babd22a6808c428c4d30d4
cncnewton37@gmail.com <cncnewton37@gmail.com>
2018-10-02 09:51:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = 0;    //only to initiate do while and while loop, goes outside
+
+
+//        while (number < 15) {
+//            System.out.println("Enter a number between 1 and 15");
+//            int input = in.nextInt();
+//            System.out.println("Millimeters: " + input * 25.4);
+//            number++;
+
+
+//      Do While: loops many times
+//        do {
+//            System.out.println("Enter a number between 1 and 15");
+//            int input = in.nextInt();
+//            System.out.println("Millimeters: " + 25.4 * input);
+//            number++;
+//        }
+//        while (number <= 15);
+
+
+//      For loop: only loops once
+//        for (int number=1; number<= 15; number++);
+//        System.out.println("Enter a number between 1 and 15");
+//        int input = in.nextInt();
+//        System.out.println("Millimeters: " + 25.4* input);
+        }
+    }
+
+}
